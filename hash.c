4a6f1617cb589a5b5a055b38a50ea69892f8d469
Aditya Laddha <f2016038@pilani.bits-pilani.ac.in>
2019-02-26 22:53:11 +0530

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,4 +1,4 @@
-#include "code.h"
+#include "hash.h"
 
 int hashcode(char* string){
 	int hash = 3;
@@ -87,28 +87,4 @@ void print2(dl d){
 		print(temp);
 		temp = temp->next1;
 	}
-}
-int main(){
-	hashtable* ht = createHashTable();
-	// ll ls = createLinkedList();
-	// ll ls2 = createLinkedList();
-
-	// node n1 = createNode(12);
-	// node n2 = createNode(28);
-	// node n3 = createNode(15);
-	// ls = insertInList(ls,n1);
-	// ls = insertInList(ls,n2);
-	// ls2 = insertInList(ls2,n3);
-	// //ls2 = insertInList(ls2,n2);
-	// dl d = createListofList();
-	// d = insertInListOfList(d,ls);
-	// //print(d->head);
-	// //ls = insertInList(ls,n3);
-	// //printf("\n");
-	// d = insertInListOfList(d,ls2);
-	// print2(d);
-	//if(d->head->next1->next1==NULL)
-		//printf("1\n");
-	//print2(d);
-	//printf("%d\n",ls->head->data);
 }
